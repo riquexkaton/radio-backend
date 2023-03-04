d0620686ed1e1e6f0b0392887ade27af18cdb316
Enrique urdaneta <enriquealejand26@gmail.com>
2023-03-03 22:55:33 -0400

    v1

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+    cors: {
+        origin: 'https://riquexfm.netlify.app/', //aqui va el dominio del front
+        methods: ['GET', 'POST'],
+        credentials: true  // permitir solicitudes con credenciales
+    }
+  });
+
+app.use(cors())
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+
+
+  //envio y recibo de mensajes
+  socket.on('message', (data)=>{
+    socket.broadcast.emit('message',data);
+  });
+
+
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
